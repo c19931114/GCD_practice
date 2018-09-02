30d1436e8eebac570215bee4c81a579dce219e7b
Crystal <c19931114@yahoo.com.tw>
2018-09-02 14:12:26 +0800

    print async in semapore way (依序顯示)

--- GCD/GCD/ViewController.swift
+++ GCD/GCD/ViewController.swift
@@ -14,50 +14,16 @@ class ViewController: UIViewController {
     
     let client = APIClient()
     var info = ["吃飯", "睡覺", "打東東"]
-    var name: String?
-    var address: String?
-    var head: String?
+    
+    var semaphore = DispatchSemaphore(value: 1)
+    // If you do this in the main thread (value: 0), the whole app will maybe freeze
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setcell()
-        
-        client.getName { (name, error) in
-
-            if let error = error {
-                print(error)
-            } else {
-                guard let name = name else { return }
-                self.info[0] = name
-                self.myTableView.reloadData()
-
-            }
-        }
-        
-        client.getAddress { (address, error) in
-            
-            if let error = error {
-                print(error)
-            } else {
-                guard let address = address else { return }
-                self.info[1] = address
-                self.myTableView.reloadData()
-
-            }
-        }
-        
-        client.getHead { (head, error) in
-
-            if let error = error {
-                print(error)
-            } else {
-                guard let head = head else { return }
-                self.info[2] = head
-                self.myTableView.reloadData()
-
-            }
-        }
+        asyncPrint()
+    
         
     }
 
@@ -69,6 +35,83 @@ class ViewController: UIViewController {
         
         let cellNib = UINib(nibName: "MyTableViewCell", bundle: nil)
         myTableView.register(cellNib, forCellReuseIdentifier: "cell")
+    }
+    
+    func asyncPrint() {
+        
+        DispatchQueue.global().async {
+            
+            print("---------------------------")
+            print(">>>>>>>>>>>> wait get name")
+            print("---------------------------")
+            self.semaphore.wait()
+
+            self.client.getName { (name, error) in
+                
+                if let error = error {
+                    
+                    print(error)
+                    self.semaphore.signal()
+                } else {
+                    
+                    guard let name = name else { return }
+                    self.info[0] = name
+                    self.myTableView.reloadData()
+                    
+                    print("---------------------------")
+                    print(">>>>>>>>>>>> get name")
+                    print("---------------------------")
+                    self.semaphore.signal()
+                    
+                }
+            }
+            
+            print("---------------------------")
+            print(">>>>>>>>>>>> wait get address")
+            print("---------------------------")
+            self.semaphore.wait()
+            
+            
+            self.client.getAddress { (address, error) in
+                
+                if let error = error {
+                    
+                    print(error)
+                    self.semaphore.signal()
+                } else {
+                    
+                    guard let address = address else { return }
+                    self.info[1] = address
+                    self.myTableView.reloadData()
+                    
+                    print("---------------------------")
+                    print(">>>>>>>>>>>> get address")
+                    print("---------------------------")
+                    self.semaphore.signal()
+                    
+                }
+            }
+            
+            print("---------------------------")
+            print(">>>>>>>>>>>> wait get head")
+            print("---------------------------")
+            self.semaphore.wait()
+            
+            self.client.getHead { (head, error) in
+                
+                if let error = error {
+                    
+                    print(error)
+                } else {
+                    
+                    guard let head = head else { return }
+                    self.info[2] = head
+                    self.myTableView.reloadData()
+                }
+            }
+            
+        }
+        
     }
 
 }
@@ -90,19 +133,13 @@ extension ViewController: UITableViewDataSource {
         if let cell = tableView.dequeueReusableCell(
             withIdentifier: "cell",
             for: indexPath) as? MyTableViewCell {
-            
-            //let cellIndexPath = info[indexPath.row]
-            
+                        
             cell.infoLabel.text = info[indexPath.row]
             
             return cell
         }
         return UITableViewCell()
     }
-    
-}
-
-extension ViewController {
     
 }
 
